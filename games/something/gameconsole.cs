9bfb4689238af39630c959d4392640d5c98b4656
ac146989@avcol.school.nz <@avcol.school.nz>
2025-06-10 14:15:03 +1200

    added a class

---
+++ games/something/gameconsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace games.something
+{
+    internal class gameconsole
+    {
+        public char games_name {  get; set; }
+
+        public int games_id { get; set; }
+
+        public gameconsole (char ingamesname, int ingamesid)
+        {
+
+            games_id = ingamesid;
+
+            games_name = ingamesname;
+
+        }
+    }
+}
