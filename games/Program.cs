d919a427d551ff76bf89c182c7955ef5ea369ee9
ac146989@avcol.school.nz <@avcol.school.nz>
2025-07-21 15:03:17 +1200

    dots

--- games/Program.cs
+++ games/Program.cs
@@ -14,7 +14,7 @@ namespace games
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello there");
+            Console.WriteLine(".........");
             string connectionstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=console;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
             storemanager = new StoreManager(connectionstring);
